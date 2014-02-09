cb44d2a25c4d9772878131b994794c187eb6b01f
Alex Yang <alexyang.personal@gmail.com>
2014-02-09 10:58:25 -0800

    Alex's changes

--- app/controllers/interface_controller.rb
+++ app/controllers/interface_controller.rb
@@ -1,5 +1,23 @@
 class InterfaceController < ApplicationController
-  def form
+  def form1
+  end
+
+  def form2
+  end
+
+  def form3
+  end
+
+  def form4
+  end
+
+  def form5
+  end
+
+  def form6
+  end
+
+  def form7
   end
 
   def thanks
